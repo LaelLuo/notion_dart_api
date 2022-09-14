380b17eb43c529365ab679d715a867e39272bf7c
LaelLuo <laelluo@qq.com>
2022-09-15 01:28:42 +0800

    :sparkles: 解析 Database properties

--- lib/src/gen/model/properties.dart
+++ lib/src/gen/model/properties.dart
@@ -1,17 +1,23 @@
+import './property.dart';
 import '../../notion.dart';
 
 class Properties {
-  final Map<String, dynamic> data;
+  final Map<String, Property> data;
 
   Notion? notion;
 
   Properties(this.data);
 
   factory Properties.fromJson(Map<String, dynamic> json) {
-    return Properties(json);
+    return Properties({
+      for (final e in json.entries) e.key: Property.fromJson(e.value),
+    });
   }
-
-  Map<String, dynamic> toJson() => data;
+  Map<String, dynamic> toJson() {
+    return {
+      for (final e in data.entries) e.key: e.value.toJson(),
+    };
+  }
 
   Properties copy() => Properties.fromJson(toJson());
 }
